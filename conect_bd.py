757717d02e08c5460028b0955dc42ca1b5f6f017
Vladimir Gualberto <vladimirgualberto@gmail.com>
2021-01-27 11:40:18 -0300

    Update conect_bd.py

--- conect_bd.py
+++ conect_bd.py
@@ -4,8 +4,7 @@ import pandas as pd
 
 
 def conectabd(ip, data):
-    engine = sqlalchemy.create_engine( 'mysql+pymysql://nipcdr:cdrpass2.0@'+ip+':3306/nipcdr')
-    #sql = "SELECT calldate, src, realdst,disposition, duration FROM nipcdr.a_cdr where calldate between '2020-"+mes+"-28' and '2020-"+mes+"-28'"
+    engine = sqlalchemy.create_engine( 'mysql+pymysql://user:password@'+ip+':3306/database')
     sql = "SELECT calldate, src, realdst,disposition, duration FROM nipcdr.a_cdr where calldate >= '"+data+" 00:00:01' and calldate <= '"+data+" 23:59:59'"
     df = pd.read_sql(sql,engine)
     return df
